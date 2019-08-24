c59cf5bd3dd6acb93dfc57c7d660af646362c58d
irawanria85 <irawanria85@gmail.com>
2019-08-24 17:40:29 +0700

    Update gotoayo.js

--- gotoayo.js
+++ gotoayo.js
@@ -1,3 +1,11 @@
+/* Pergi ke Laman Ayo */
 function eventualRedirect(redirectTo, timeoutPeriod) {
 setTimeout("location.href = redirectTo;",timeoutPeriod);}
 setTimeout("location.href = '/p/ayo.html';",20000);
+/* Jika mengunakan PC */
+if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
+else location.replace("https://keepopedia.blogspot.com/p/tunggu-sebentar.html");
+/* Tekan tombol Back */
+history.pushState(null,null,'#');
+window.addEventListener('popstate',function(event)
+{history.pushState(null,null,'#');});
