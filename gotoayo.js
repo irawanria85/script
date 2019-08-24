20a5c8ee70bcc2058aa6a60f9f0e819b37ec1779
irawanria85 <irawanria85@gmail.com>
2019-08-24 17:53:02 +0700

    Update gotoayo.js

--- gotoayo.js
+++ gotoayo.js
@@ -4,7 +4,7 @@ setTimeout("location.href = redirectTo;",timeoutPeriod);}
 setTimeout("location.href = '/p/ayo.html';",20000);
 /* JIKA MENGGUNAKAN PC */
 if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
-else location.replace("/p/tunggu-sebentar.html");
+else location.replace("/p/please-wait.html");
 /* ANTI BACK */
 history.pushState(null,null,'#');
 window.addEventListener('popstate',function(event)
