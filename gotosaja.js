829bf349a59d68ddc146464c31442bba0576e129
irawanria85 <irawanria85@gmail.com>
2019-08-24 17:54:46 +0700

    Create gotosaja.js

---
+++ gotosaja.js
@@ -0,0 +1,11 @@
+/* PERGI KE LAMAN SEBENTAR */
+function eventualRedirect(redirectTo, timeoutPeriod) {
+setTimeout("location.href = redirectTo;",timeoutPeriod);}
+setTimeout("location.href = '/p/saja.html';",20000);
+/* JIKA MENGGUNAKAN PC */
+if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
+else location.replace("/p/please-wait.html");
+/* ANTI BACK */
+history.pushState(null,null,'#');
+window.addEventListener('popstate',function(event)
+{history.pushState(null,null,'#');});
