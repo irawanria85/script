f574fa0db06621da5894c78a7eb2088edac0ab59
irawanria85 <irawanria85@gmail.com>
2019-08-25 17:31:46 +0700

    Update pergikepleasewait.js

--- pergikepleasewait.js
+++ pergikepleasewait.js
@@ -1,7 +1,7 @@
 /* PERGI KE LAMAN */
 function eventualRedirect(redirectTo, timeoutPeriod) {
 setTimeout("location.href = redirectTo;",timeoutPeriod);}
-setTimeout("location.href = '/p/please-wait.html';",25000);
+setTimeout("location.href = '/p/please-wait.html';",20000);
 /* JIKA MENGGUNAKAN PC */
 if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
 else location.replace("//www.indonesiya.com/p/please-wait.html");
