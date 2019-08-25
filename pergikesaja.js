ddea14a007965f64369e30194fd3ea2f9392d898
irawanria85 <irawanria85@gmail.com>
2019-08-25 17:28:35 +0700

    Update pergikesaja.js

--- pergikesaja.js
+++ pergikesaja.js
@@ -1,7 +1,7 @@
 /* PERGI KE LAMAN SAJA */
 function eventualRedirect(redirectTo, timeoutPeriod) {
 setTimeout("location.href = redirectTo;",timeoutPeriod);}
-setTimeout("location.href = '/p/saja.html';",25000);
+setTimeout("location.href = '/p/saja.html';",20000);
 /* JIKA MENGGUNAKAN PC */
 if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
 else location.replace("//www.indonesiya.com/p/please-wait.html");
