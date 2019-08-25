c31700ed1fd250076efe29c857fcaa8a50b2c2df
irawanria85 <irawanria85@gmail.com>
2019-08-25 16:55:38 +0700

    Create pergikegandos.js

---
+++ pergikegandos.js
@@ -0,0 +1,11 @@
+* PERGI KE LAMAN GANDOS */
+function eventualRedirect(redirectTo, timeoutPeriod) {
+setTimeout("location.href = redirectTo;",timeoutPeriod);}
+setTimeout("location.href = '/p/gandos.html';",25000);
+/* JIKA MENGGUNAKAN PC */
+if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
+else location.replace("//www.indonesiya.com/p/please-wait.html");
+/* ANTI BACK */
+history.pushState(null,null,'#');
+window.addEventListener('popstate',function(event)
+{history.pushState(null,null,'#');});
