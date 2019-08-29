2d6eb1abb0c8ddca732784979cd84fd9688798e0
irawanria85 <irawanria85@gmail.com>
2019-08-29 20:28:21 +0700

    Update tontonvideofloat.js

--- tontonvideofloat.js
+++ tontonvideofloat.js
@@ -11,4 +11,15 @@ var dir = xf>x0 ? 1 : -1;
 var x = x0 + dx * dir;
 gb.style.top = x.toString() + "px";
 if(x0!=xf){setTimeout("moveGB("+x+", "+xf+")", 10);}
-}
+};
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname+"#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if(location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function(){
+                location.replace("https://www.aridjaya.com/p/play-video.html");
+            },0);
+        }}, false);
+}(window, location));
