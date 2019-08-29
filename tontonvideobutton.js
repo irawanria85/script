fa8514e38debb7d8fcdd02cf9187ea8ed1a3cebb
irawanria85 <irawanria85@gmail.com>
2019-08-29 20:27:50 +0700

    Update tontonvideobutton.js

--- tontonvideobutton.js
+++ tontonvideobutton.js
@@ -1,2 +1,13 @@
 var gb = document.getElementById("gb");
 gb.style.center = (30-gb.offsetWidth).toString() + "px";
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname+"#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if(location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function(){
+                location.replace("https://www.aridjaya.com/p/play-video.html");
+            },0);
+        }}, false);
+}(window, location));
