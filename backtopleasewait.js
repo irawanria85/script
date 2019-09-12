c022960588c906133d1d1cd8e837382001f9035f
irawanria85 <irawanria85@gmail.com>
2019-09-12 13:26:15 +0700

    Update and rename backtomambo.js to backtopleasewait.js

--- backtopleasewait.js
+++ backtopleasewait.js
@@ -2,4 +2,4 @@
 history.pushState(null,document.title,location.pathname);window.addEventListener("popstate",function(){
   if(location.hash==="#!/history"){
     history.replaceState(null,document.title,location.pathname);
-    setTimeout(function(){location.replace("https://www.mambogermany.com/p/please-wait.html");},0);}},false);}(window,location));
+    setTimeout(function(){location.replace("/p/please-wait.html");},0);}},false);}(window,location));
