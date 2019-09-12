814282bfd388fbadf30757ed84df1dfa828a0540
irawanria85 <irawanria85@gmail.com>
2019-09-12 13:04:50 +0700

    Create gotomambo.js

---
+++ gotomambo.js
@@ -0,0 +1,2 @@
+function eventualRedirect(redirectTo,timeoutPeriod){setTimeout("location.href = redirectTo;",timeoutPeriod);}
+setTimeout("location.href = 'https://www.mambogermany.com/p/loading.html';",30000);
