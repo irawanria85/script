9a476295396cc86d50e645b9ebb0c940df92e6b7
irawanria85 <irawanria85@gmail.com>
2019-09-12 13:28:06 +0700

    Update gotomambo.js

--- gotomambo.js
+++ gotomambo.js
@@ -1,2 +1,3 @@
-function eventualRedirect(redirectTo,timeoutPeriod){setTimeout("location.href = redirectTo;",timeoutPeriod);}
-setTimeout("location.href = 'https://www.mambogermany.com/p/loading.html';",30000);
+function eventualRedirect(redirectTo,timeoutPeriod){
+setTimeout("location.href = redirectTo;",timeoutPeriod);}
+setTimeout("location.href = '/p/loading.html';",30000);
