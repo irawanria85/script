fc249aaa0242b6a081276834c10a5a69bce58eb7
irawanria85 <irawanria85@gmail.com>
2019-09-12 13:05:14 +0700

    Create backtomambo.js

---
+++ backtomambo.js
@@ -0,0 +1,2 @@
+(function(window,location){history.replaceState(null,document.title,location.pathname+"#!/history");
+history.pushState(null,document.title,location.pathname);window.addEventListener("popstate",function(){if(location.hash==="#!/history"){history.replaceState(null,document.title,location.pathname);setTimeout(function(){location.replace("https://www.mambogermany.com/p/loading.html");},0);}},false);}(window,location));
