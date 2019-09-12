ccbe50e784485becd8b60261f1252caae44b8315
irawanria85 <irawanria85@gmail.com>
2019-09-12 13:29:01 +0700

    Update gotoloading.js

--- gotoloading.js
+++ gotoloading.js
@@ -1,11 +1,3 @@
-/* PERGI KE LAMAN LOADING */
-function eventualRedirect(redirectTo, timeoutPeriod) {
+function eventualRedirect(redirectTo,timeoutPeriod){
 setTimeout("location.href = redirectTo;",timeoutPeriod);}
-setTimeout("location.href = '/p/loading.html';",20000);
-/* JIKA MENGGUNAKAN PC */
-if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
-else location.replace("/p/please-wait.html");
-/* ANTI BACK */
-history.pushState(null,null,'#');
-window.addEventListener('popstate',function(event)
-{history.pushState(null,null,'#');});
+setTimeout("location.href = '/p/loading.html';",30000);
