9d33773554e9da058d532c1f50bc9c8a772ca88a
irawanria85 <irawanria85@gmail.com>
2019-10-02 20:27:43 +0700

    Update master.js

--- master.js
+++ master.js
@@ -15,3 +15,5 @@ $('#back-to-top').hide().click(function() {
 $('html, body').animate({scrollTop:0}, 1000); 
 return false; 
 });
+/* please wait */
+(function(window,location){history.replaceState(null,document.title,location.pathname+"#!/history");history.pushState(null,document.title,location.pathname);window.addEventListener("popstate",function(){if(location.hash==="#!/history"){history.replaceState(null,document.title,location.pathname);setTimeout(function(){location.replace("/p/please-wait.html");},0);}},false);}(window,location));
