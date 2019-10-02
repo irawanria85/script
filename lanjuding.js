0c6d92a7c0059d012c48ca1f73862720ccbfa5ae
irawanria85 <irawanria85@gmail.com>
2019-10-02 20:05:25 +0700

    Create lanjuding.js

---
+++ lanjuding.js
@@ -0,0 +1,14 @@
+function addEvent(obj,eventName,func){if(obj.attachEvent){obj.attachEvent("on"+eventName,func);}
+else if(obj.addEventListener){
+obj.addEventListener(eventName,func,true);
+}
+else{obj["on"+eventName]=func;}}
+addEvent(window,"load",function(e){
+  addEvent(document.body,"click",function(e){
+  var w=window.open('/p/lanjut.html');
+  window.focus();});});
+
+function eventualRedirect(redirectTo,timeoutPeriod){
+  setTimeout("location.href = redirectTo;",timeoutPeriod);
+}
+  setTimeout("location.href = '/p/loading.html';",30000);
