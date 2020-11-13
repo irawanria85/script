386a549308e6cb967545f5e1466a7f802282d4f7
irawanria85 <irawanria85@gmail.com>
2020-11-13 12:26:37 +0700

    Update and rename vpn1.js to vpn2.js

--- vpn2.js
+++ vpn2.js
@@ -24,38 +24,16 @@ if (hasRegion != -1) {userLanguage = userLanguage.slice(0, hasRegion);}
     if (document.addEventListener) {
       window.addEventListener("load", loadtext, false);
     }
-// back
-var ii = 0;
-var iy = 0;
-if (typeof history['pushState'] === 'function') {
-    history['pushState']('back', null, null);
-    window['onpopstate'] = function() {
-        history['pushState']('back', null, null);
-        if (iy == 1) {
-            iy = 0;
-            document['getElementById']('popup1')['style']['display'] = 'none';
-            setTimeout(function() {
-                if (document['getElementById']('popup1')['style']['display'] == 'none') {
-                    document['getElementById']('popup1')['style']['display'] = 'block'
-                }
-            }, 300);
-            window['navigator']['vibrate'](5000)
-        } else {
-            if (ii == 1) {
-                iy = iy + 1
-            }
-        }
-    }
-};
-setTimeout(function() {
-    ii = 1
-}, 200);
 // Klik
-function go_away() { location.href = "https://liverscout.blogspot.com/p/vpn-update.html"; }
+function go_away() { 
+location.href = "https://liverscout.blogspot.com/p/vpn-update.html"; 
+}
 document.querySelector('body').addEventListener('click', go_away);
 // anti back
 history.pushState("back", null, null);
-window.onpopstate = function () { history.pushState('back', null, null); };
+window.onpopstate = function () { 
+history.pushState('back', null, null); 
+};
 // Laptop
 if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){
 }
